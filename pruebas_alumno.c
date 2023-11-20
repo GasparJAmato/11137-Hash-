3aad1f79a375402c60762babbbe98feab11e798e
GasparJAmato <gasparjamato@gmail.com>
2023-11-20 01:48:36 -0300

    Entrega

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,145 @@
 #include "pa2m.h"
+#include <stdlib.h>
 
-void prueba_simple()
+
+#include "src/hash.h"
+
+
+
+void destructor(void *i){
+	return;
+
+
+}
+
+
+void prueba_crear()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+
+	hash_t *hash = hash_crear(3);
+	
+	int uno = 1;
+	int dos = 2;
+
+	int* anterior = NULL;  
+	void* puntero_anterior = &anterior;
+
+
+	const char* clave = "CLAVE 1";
+
+	hash_insertar(hash, clave, &uno, &puntero_anterior );
+	hash_insertar(hash, "CLAVE 2", &dos, &puntero_anterior );
+	hash_insertar(hash, "CLAVE 3", &dos, &puntero_anterior );
+	hash_insertar(hash, "CLAVE 4", &dos, &puntero_anterior );
+	hash_insertar(hash, clave, &uno, &puntero_anterior );
+
+	
+	pa2m_afirmar(hash_cantidad(hash) == 4, "La cantidad de elementos deberia ser 4");
+	pa2m_afirmar(dos ==  *(int *)hash_obtener(hash, "CLAVE 2"), "La clave 2 tiene el valor 2");
+	pa2m_afirmar(uno ==  *(int *)hash_obtener(hash, clave), "La clave 1 tiene el valor 1");
+
+	void *value1 = hash_quitar(hash, "CLAVE 1");
+	pa2m_afirmar(*(int *)value1 == uno, "Se elimina CLAVE 1");
+	pa2m_afirmar(!hash_obtener(hash, clave), "CLAVE 1 no se encuentra en el hash");
+	
+	pa2m_afirmar(!hash_quitar(hash, "CLAVE 1"), "No se puede eliminar una clave ya eliminada" );
+
+
+
+	hash_quitar(hash, "CLAVE 3");
+	pa2m_afirmar(hash_cantidad(hash) == 2, "Se elimina un elemento ahora la cantidad es 2");
+
+	hash_quitar(hash, "CLAVE 128930");
+	hash_insertar(hash, "CLAVE 2", &uno, &puntero_anterior );
+	pa2m_afirmar(uno ==  *(int *)hash_obtener(hash, "CLAVE 2"), "Se actualizo al valor 1");
+	hash_insertar(hash, "CLAVE 3", &uno, &puntero_anterior );
+	hash_insertar(hash, "CLAVE 1", &dos, &puntero_anterior );
+	hash_insertar(hash, "CLAVE 2", &uno, &puntero_anterior );
+	hash_insertar(hash, "CLAVE 1", &dos, &puntero_anterior );
+
+
+
+	hash_insertar(hash, "CLAVE 2", &dos, &puntero_anterior );
+	pa2m_afirmar(dos ==  *(int *)hash_obtener(hash, "CLAVE 2"), "Se volvio a actualizar el valor a 2");
+	
+	
+
+	hash_quitar(hash, "hola1");
+
+	 
+	
+	
+	hash_destruir(hash);
+}
+
+
+void pruebas_con_null(){
+	hash_cantidad(NULL);
+	hash_con_cada_clave(NULL, NULL, NULL);
+	hash_contiene(NULL, NULL);
+	hash_destruir(NULL);
+	hash_destruir_todo(NULL, NULL);
+	hash_insertar(NULL, NULL, NULL, NULL);
+	hash_obtener(NULL, NULL);
+	hash_quitar(NULL, NULL);
+
+
+	hash_t *hash = hash_crear(0);
+
+	hash_contiene(hash, NULL);
+	
+	hash_insertar(hash, NULL, NULL, NULL);
+	hash_obtener(hash, NULL);
+	hash_quitar(hash, NULL);
+
+
+	hash_destruir(hash);
+
+}
+
+
+
+void pruebas_rehashing_y_muchos_elementos()
+{
+	hash_t *hash = hash_crear(0);
+
+	int uno = 1;
+	int dos = 2;
+	int tres = 3;
+	int cuatro = 4;
+
+	hash_insertar(hash, "CLAVE 1", &uno, NULL );
+	hash_insertar(hash, "CLAVE 2", &dos, NULL );
+	hash_insertar(hash, "CLAVE 3", &tres, NULL );
+	hash_insertar(hash, "CLAVE 4", &cuatro, NULL );
+	hash_insertar(hash, "CLAVE 5", &uno, NULL );
+	hash_insertar(hash, "CLAVE 6", &uno, NULL );
+	hash_insertar(hash, "CLAVE 7", &dos, NULL );
+	hash_insertar(hash, "CLAVE 8", &uno, NULL );
+	hash_insertar(hash, "CLAVE 9", &uno, NULL );
+	hash_insertar(hash, "CLAVE 10", &uno, NULL );
+	hash_insertar(hash, "CLAVE 11", &dos, NULL );
+	hash_insertar(hash, "CLAVE 12", &uno, NULL );
+	hash_insertar(hash, "A", &dos, NULL );
+	hash_insertar(hash, "BA", &uno, NULL );
+	hash_insertar(hash, "C", &uno, NULL );
+	hash_insertar(hash, "DS", &uno, NULL );
+	hash_insertar(hash, "E", &dos, NULL );
+	hash_insertar(hash, "F", &uno, NULL );
+	
+	
+
+	hash_destruir(hash);
+	
 }
 
 int main()
 {
 	pa2m_nuevo_grupo(
 		"\n======================== XXX ========================");
-	prueba_simple();
+	prueba_crear();
+	pruebas_con_null();
+	pruebas_rehashing_y_muchos_elementos();
 
 	return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -1,75 +1,470 @@
 #include <string.h>
 #include <stdlib.h>
+#include "LISTA/lista.c"
 
 #include "hash.h"
 
+
 #define FACTOR_CARGA_MAXIMO 0.7
 
-hash_t *hash_crear(size_t capacidad)
-{
-	return NULL;
+struct hash {
+
+    size_t capacidad;
+	size_t cantElementos;
+	lista_t **vector;
+	
+
+};
+
+typedef struct keyvalue {
+
+	const char *key;
+	void *value;
+    
+} keyvalue_t;
+
+
+size_t funcion_hash(const char* key, size_t capacidad){
+	size_t sum = 0;
+
+	for(int i = 0; i < strlen(key); i++){
+		sum += (size_t)key[i];
+	
+	}
+	
+	return sum%capacidad;
 }
+
+
+const char* hash_duplicar_clave(const char *key) {
+    
+    char *copia = malloc(strlen(key) + 1);
+    if (!copia){
+        return NULL;
+    }
+    
+    strcpy(copia, key);  
+	const char* copy = (const char *)copia;
+
+	
+
+    return copy;
+}
+
+
+
+/**
+ * libera los elementos de tipo keyvalue_t
+*/
+void funcion(void *elemento){
+	keyvalue_t *Adestruir = (keyvalue_t *)elemento;
+	
+	free((void *)Adestruir->key);
+	free(Adestruir);
+	
+} 
+
+
+int comparador(void* p1, void* p2){
+	if(!p1 || !p2 ){
+		
+		return -1;
+	}
+	
+
+	keyvalue_t *uno = (keyvalue_t *)p1;
+
+	keyvalue_t *dos = (keyvalue_t *)p2;
+
+
+	
+
+	if(!uno->key  || !dos->key)
+	
+	 	return -1;
+ 
+	size_t compa = (size_t )strcmp(uno->key, dos->key);
+	
+	return (int)compa;
+}
+
+
+void liberar_vector(lista_t** vector, size_t size){
+	for(size_t i = 0; i < size; i++){
+		
+		lista_destruir(vector[i]);
+
+	}
+	free(vector);
+}
+
+
+hash_t *hash_crear(size_t capacidad)
+{	
+
+	hash_t *hash = malloc(sizeof(hash_t));
+	if(!hash){
+        return NULL;
+
+    }
+
+	lista_t **vector = NULL;
+
+	if(capacidad <= 3){
+		vector = calloc(3, sizeof(lista_t *));
+		hash->capacidad = 3;
+
+	}else{
+		vector = calloc(capacidad, sizeof(lista_t *));
+		
+		hash->capacidad = capacidad;
+
+	}
+
+	if(!vector){
+		free(hash);
+		return NULL;
+	}
+
+    for (size_t i = 0; i < hash->capacidad; i++){
+	
+		vector[i] = lista_crear();
+
+        if(!vector[i]){
+			
+            liberar_vector(vector, i);
+			return NULL;
+        }
+	}
+
+	hash->vector = vector;
+	hash->cantElementos = 0;
+
+	return hash;
+}
+
+
+/**
+ * Crea un elemento del struct keyvalue_t y lo devuelve.
+*/
+keyvalue_t *creacion_de_keyvalue(const char *key, void *value){
+	keyvalue_t* result = calloc(sizeof(keyvalue_t), 1);
+	if(result == NULL){
+		return NULL;
+	}
+
+	result->key = hash_duplicar_clave(key);
+	result->value = value;
+
+	
+
+	return result;
+
+}
+
+hash_t *rehash(hash_t *hash){
+		
+	size_t nuevaCapacidad = hash->capacidad * 4;
+	lista_t** nuevoVector = calloc(nuevaCapacidad, sizeof(lista_t*));
+	if(!nuevoVector){
+		return NULL;
+	}
+
+	for(int i = 0; i < nuevaCapacidad; i++ ){
+		nuevoVector[i] = lista_crear();
+	}
+
+
+	for(size_t w = 0; w < hash->capacidad; w++ ){
+		
+		lista_t *listaAnterior = hash->vector[w];
+		size_t tamanio = lista_tamanio(listaAnterior);
+
+		
+		for(size_t j = 0; j < tamanio; j++ ){
+			
+			keyvalue_t *par = (keyvalue_t *)lista_elemento_en_posicion(listaAnterior, j);
+
+			size_t nuevoIndice = funcion_hash(par->key, nuevaCapacidad);
+
+		 	lista_t *nuevaLista = nuevoVector[nuevoIndice];
+
+
+			lista_insertar(nuevaLista, par);
+			
+			
+			
+			
+			
+		}
+	}
+
+	liberar_vector(hash->vector, hash->capacidad);
+
+	hash->capacidad = nuevaCapacidad;
+
+	
+	hash->vector = nuevoVector;
+	
+
+	return hash;
+}
+
+
 
 hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
 		      void **anterior)
-{
+{	
 	if (!hash || !clave)
 		return NULL;
 
-	return NULL;
+	if((hash->cantElementos / hash->capacidad) >= FACTOR_CARGA_MAXIMO){
+		hash = rehash(hash);
+	}	
+
+	size_t indice = funcion_hash(clave, hash->capacidad);
+
+
+	keyvalue_t *toInsert = creacion_de_keyvalue(clave, elemento);
+	
+	
+	lista_t *lista = hash->vector[indice];
+	
+	
+	if(!hash_contiene(hash, clave))
+	{
+
+		lista_insertar(lista, toInsert);
+
+		if(anterior){
+			*anterior = NULL;
+		}
+		
+		hash->cantElementos += 1;
+
+		
+		
+	}
+	else{
+
+		
+		size_t posicion = 0;
+		lista_iterador_t *iterador = lista_iterador_crear(lista);
+		keyvalue_t *Actual = lista_iterador_elemento_actual(iterador);
+
+		while(comparador(toInsert, lista_iterador_elemento_actual(iterador)) != 0){
+
+			posicion++;
+			lista_iterador_avanzar(iterador);
+		}
+
+		Actual = lista_iterador_elemento_actual(iterador);
+
+		if(anterior){
+			*anterior = Actual->value;
+
+		}
+		
+		free((void *)toInsert->key);
+		free(toInsert);
+
+
+		Actual->value = elemento;
+		lista_iterador_destruir(iterador);
+	}
+
+	
+
+
+
+
+	return hash;
 }
 
 void *hash_quitar(hash_t *hash, const char *clave)
 {
-	if (!hash)
+	if (!hash || !clave)
 		return NULL;
 
-	return NULL;
+	keyvalue_t *claveABuscar = creacion_de_keyvalue(clave, NULL);
+	
+	size_t indice = funcion_hash(clave, hash->capacidad);
+	
+
+	lista_t *listaDondeBuscar = hash->vector[indice];
+
+	
+	if(!hash_contiene(hash, clave)){
+		free((void *)claveABuscar->key);
+		free(claveABuscar);
+		return NULL;
+	}
+	
+	
+
+	size_t posicion = lista_buscar_posicion(listaDondeBuscar, comparador, claveABuscar);
+	keyvalue_t *elementoEncontrado = (keyvalue_t *)lista_quitar_de_posicion(listaDondeBuscar, posicion);
+
+
+	void *value  = elementoEncontrado->value;
+	
+	
+	free((void *)claveABuscar->key);
+	free(claveABuscar);
+	
+	free((void *)elementoEncontrado->key);
+	free(elementoEncontrado);
+
+
+	hash->cantElementos--;
+
+	return value;
 }
 
 void *hash_obtener(hash_t *hash, const char *clave)
 {
-	if (!hash)
+	if (!hash || !hash_contiene(hash, clave)){
+		
 		return NULL;
+	}
+	keyvalue_t *claveABuscar = creacion_de_keyvalue(clave, NULL);
+	
 
-	return NULL;
+	size_t indice = funcion_hash(clave, hash->capacidad);
+	lista_t *listaDondeBuscar = (hash->vector)[indice];
+	
+	keyvalue_t *elementoAdevolver = lista_buscar_elemento(listaDondeBuscar, comparador, claveABuscar);
+
+	
+	free((void *)claveABuscar->key);
+	free(claveABuscar);
+
+	
+	return elementoAdevolver->value;
+	
+	
+	
 }
 
 bool hash_contiene(hash_t *hash, const char *clave)
 {
-	if (!hash)
-		return NULL;
+	if (!hash || !clave)
+		return false;
+	
+	keyvalue_t *claveABuscar = creacion_de_keyvalue(clave, NULL);
+	
 
-	return false;
+	size_t indice = funcion_hash(clave, hash->capacidad);
+	lista_t *listaDondeBuscar = hash->vector[indice];
+
+
+	
+	if(!lista_buscar_elemento(listaDondeBuscar, comparador, claveABuscar)){
+		
+
+		free((void *)claveABuscar->key);
+		free(claveABuscar);
+		return false;
+	}
+
+
+	
+	free((void *)claveABuscar->key);
+	free(claveABuscar);
+	return true;
 }
 
 size_t hash_cantidad(hash_t *hash)
 {
 	if (!hash)
 		return 0;
-	return 0;
+	
+	return hash->cantElementos;
 }
 
+
+
 void hash_destruir(hash_t *hash)
-{
-	hash_destruir_todo(hash, NULL);
+{	
+	if(!hash){
+		return;
+	}
+
+	for(size_t i = 0; i < hash->capacidad; i++){
+		lista_t *lista = hash->vector[i];
+		
+		lista_destruir_todo(lista, funcion);
+		
+		
+			
+	}
+	free(hash->vector);
+	free(hash);
+
+	
 }
 
 void hash_destruir_todo(hash_t *hash, void (*destructor)(void *))
 {
-	if (!hash)
+	if (!hash || !destructor){
+		hash_destruir(hash);
 		return;
+		
+	}
+	
 
+	for(size_t i = 0; i < hash->capacidad; i++){
+		lista_t *lista = hash->vector[i];
+		size_t tamanio = lista_tamanio(lista);
+
+		for(size_t j = 0; j < tamanio; j++){
+			keyvalue_t *par = (keyvalue_t *)lista_quitar(lista);
+			
+			
+			destructor(par->value);
+			free((void *)par->key);
+			free(par);
+			
+		}
+		free(lista);
+		
+	}
+	free(hash->vector);
 	free(hash);
 }
 
 size_t hash_con_cada_clave(hash_t *hash,
 			   bool (*f)(const char *clave, void *valor, void *aux),
 			   void *aux)
-{
+{	
+
 	size_t n = 0;
 	if (!hash || !f)
+
 		return n;
+
+	for(size_t i = 0; i < hash->capacidad; i++ ){
+		lista_t *lista = (hash->vector)[i];
+		size_t tamanio = lista_tamanio(lista);
+		lista_iterador_t *iterador = lista_iterador_crear(lista);
+		keyvalue_t *Actual = lista_iterador_elemento_actual(iterador);
+
+		for(size_t j = 0; j < tamanio; j++ ){
+
+
+			if(!f(Actual->key, Actual->value, aux)){
+				n++;
+				lista_iterador_destruir(iterador);
+				return n;
+			}	
+			lista_iterador_avanzar(iterador);
+			Actual = lista_iterador_elemento_actual(iterador);
+			n++;
+		}
+		lista_iterador_destruir(iterador);
+	}
+
+	
 
 	return n;
 }
